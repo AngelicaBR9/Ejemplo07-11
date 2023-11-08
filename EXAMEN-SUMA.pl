9a6454b54badf01b3b61253702291ad11d8f8b9a
AngelicaBR9 <angelica.burgos.ramos77999@gmail.com>
2023-11-07 20:49:51 -0400

    primer comentario

---
+++ EXAMEN-SUMA.pl
@@ -0,0 +1,6 @@
+% Predicado para sumar los dígitos de un número.
+sumar_digitos(0, 0). 
+sumar_digitos(N, Suma) :-
+    N > 0,
+    Digito is N mod 10,  Resto is N // 10,  sumar_digitos(Resto, SumaResto),  
+    Suma is SumaResto + Digito.     
---
+++ invertir.pl
@@ -0,0 +1,8 @@
+invertir_digitos(Numero, Invertido) :-
+    invertir_digitos_aux(Numero, 0, Invertido).
+invertir_digitos_aux(0, Acumulador, Acumulador).
+invertir_digitos_aux(Numero, Acumulador, Invertido) :-
+    Digito is Numero mod 10,      
+    NuevoNumero is Numero // 10, 
+    NuevoAcumulador is Acumulador * 10 + Digito, 
+    invertir_digitos_aux(NuevoNumero, NuevoAcumulador, Invertido).
